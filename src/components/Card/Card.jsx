9c951923a89dafccb99cffc77a321778329c7dc0
Shoaib khan <shoaibmangal@yahoo.com>
2025-08-26 23:55:21 +0500

    api fetch react

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,7 @@
+import "./Card.css";
+
+function Card({ children }) {
+  return <div className="card-container">{children}</div>;
+}
+
+export default Card;
---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import Card from "../Card/Card";
+import axios from "axios";
+
+import "./Users.css";
+
+function Users() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(response?.data);
+      } catch (error) {
+        console.log(error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getUsers();
+  }, []);
+
+  if (isLoading) return <h1>Loading....</h1>;
+
+  if (isError) return <h1>Something went wrong!</h1>;
+
+  return (
+    <div>
+      {users?.map((user) => {
+        return (
+          <Card key={`user-${user?.id}`}>
+            <h2>Username: {user?.username}</h2>
+            <h1>ID: {user?.id}</h1>
+            <h2>Email: {user?.email}</h2>
+          </Card>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Users;
